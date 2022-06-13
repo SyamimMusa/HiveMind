5f3c00fa8df474c8f9fee924d6e79021afdf2590
SyamimMusa <syamimmusa19@gmail.com>
2022-06-13 08:49:36 +0800

    fix link

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>HiveMind Login</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/LogReg/login.scss">
